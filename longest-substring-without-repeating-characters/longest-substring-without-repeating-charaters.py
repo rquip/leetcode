760d6ac40a2e567768fc1ac3b0cfc749f14defa8
vpalaiokostas <rayyan.quip@gmail.com>
2024-11-02 18:29:38 -0400

    solution works given the assumptions that I made, but will need to redo the problem with the correct assumptions.

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-charaters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-charaters.py
@@ -1,17 +1,26 @@
-# Problems: Can't do any string with digits, space or symbols
+# New problem with code: I had assumed the problem is a value-sequential problem. It actually is once a symbol is repeated, the length is done -- I'm going to redo it. This solution does work given the assumptions I have.
 
 import string
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # set up alphabet comparison dictionary
-        alphabet_dict = {y:x for x,y in enumerate(string.ascii_lowercase)}
-
+        # set up alphabet/number/symbol comparison dictionary
+        if len(s) == 0: return(0)
+        alphabet_dict = {y:x+1 for x,y in enumerate(string.ascii_lowercase)}
+        misc_list = [x for x in string.punctuation]
+        misc_list.append(" ")
+        alphabet_dict.update({c:0 for c in misc_list})
+        alphabet_dict.update({str(d):0 for d in range(0,10)})
+        symbol_and_number = {e:0 for e in misc_list}
+        symbol_and_number.update({str(d):0 for d in range(0,10)})
+        
         # main algorithm
         sequence_total = 1
         largest_sum = 1
         comparer = None
         for letter_index in range(len(s)):
+            # print("character = " + s[letter_index])
+            # print("sequence_total = " + str(sequence_total))
             if letter_index == len(s)-1:
                 if largest_sum < sequence_total:
                     largest_sum = sequence_total
@@ -31,7 +40,15 @@ class Solution:
             else:
                 a = alphabet_dict[s[letter_index]]
                 b = alphabet_dict[s[letter_index+1]]
-                if comparer == "less":
+                if (s[letter_index] in symbol_and_number) and (s[letter_index + 1] in symbol_and_number) and s[letter_index] == s[letter_index + 1]:
+                    if largest_sum < sequence_total:
+                        largest_sum = sequence_total
+                        sequence_total = 1
+                    else:
+                        sequence_total = 1
+                elif ((s[letter_index] in symbol_and_number) or (s[letter_index + 1] in symbol_and_number)) and s[letter_index] != s[letter_index + 1]:
+                    sequence_total = sequence_total + 1
+                elif comparer == "less":
                     if a < b:
                         sequence_total = sequence_total + 1
                     else:
