46e193aac9716871f413452b2c5931f5f2032f6a
vpalaiokostas <rayyan.quip@gmail.com>
2024-11-02 21:57:57 -0400

    new_solution.py works!

--- longest-substring-without-repeating-characters/new_solution.py
+++ longest-substring-without-repeating-characters/new_solution.py
@@ -6,9 +6,9 @@ class Solution:
         largest_sum = 1
         iterative_value = 0
         while True:
-            if iterative_value == len(s):
-                return(largest_sum)
-            elif len(s) == 0:
+            if len(s) == 0:
+                return(0)
+            elif iterative_value == len(s):
                 return(largest_sum)
             else:
                 for x in range(iterative_value, len(s)):
@@ -16,9 +16,11 @@ class Solution:
                         if len(temporary_string) > largest_sum:
                             largest_sum = len(temporary_string)
                             iterative_value = iterative_value + 1
+                            temporary_string = ""
                             break
                         else:
                             iterative_value = iterative_value + 1
+                            temporary_string = ""
                             break
                     else:
                         temporary_string = temporary_string + s[x]
